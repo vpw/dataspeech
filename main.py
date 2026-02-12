6b30962d969d3084f3aba2e7faf24197199d99d0
Vardhan Walavalkar <vardhan.w@eternascisense.com>
2026-02-12 09:42:24 +0000

    Import dependency on pitch_apply moved to conditional.

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
 from datasets import load_dataset, Audio
 from multiprocess import set_start_method
-from dataspeech import rate_apply, pitch_apply, snr_apply, squim_apply
+from dataspeech import rate_apply, snr_apply, squim_apply
 import torch
 import argparse
 
@@ -54,6 +54,7 @@ if __name__ == "__main__":
         )
 
     if not args.avoid_pitch_computation:
+        from dataspeech import pitch_apply
         print("Compute pitch")
         pitch_dataset = dataset.cast_column(audio_column_name, Audio(sampling_rate=16_000)).map(
             pitch_apply,
